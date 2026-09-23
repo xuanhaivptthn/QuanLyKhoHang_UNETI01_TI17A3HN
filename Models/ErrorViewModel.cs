6400232cd1d2f29265beeaf6a82c4fbb8a497c0a
Viet Dung <atlasraven0@gmail.com>
2026-09-23 08:58:50 +0700

    init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QuanLyKhoHang_UNETI01_TI17A3HN.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
